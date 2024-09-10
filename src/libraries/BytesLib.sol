68b170ac17fefb5c490511d05eac0cf0ac62f4c6
ChefMist <133624774+ChefMist@users.noreply.github.com>
2024-09-10 14:42:52 +0800

    feat: s11

--- src/libraries/BytesLib.sol
+++ src/libraries/BytesLib.sol
@@ -4,8 +4,11 @@
 pragma solidity ^0.8.0;
 
 import {Constants} from "./Constants.sol";
+import {CalldataDecoder} from "pancake-v4-periphery/src/libraries/CalldataDecoder.sol";
 
 library BytesLib {
+    using CalldataDecoder for bytes;
+
     error SliceOutOfBounds();
 
     /// @notice Returns the address starting at byte 0
@@ -35,57 +38,11 @@ library BytesLib {
         }
     }
 
-    /// @notice Decode the `_arg`-th element in `_bytes` as a dynamic array
-    /// @dev The decoding of `length` and `offset` is universal,
-    /// whereas the type declaration of `res` instructs the compiler how to read it.
-    /// @param _bytes The input bytes string to slice
-    /// @param _arg The index of the argument to extract
-    /// @return length Length of the array
-    /// @return offset Pointer to the data part of the array
-    function toLengthOffset(bytes calldata _bytes, uint256 _arg)
-        internal
-        pure
-        returns (uint256 length, uint256 offset)
-    {
-        uint256 relativeOffset;
-        assembly {
-            // The offset of the `_arg`-th element is `32 * arg`, which stores the offset of the length pointer.
-            // shl(5, x) is equivalent to mul(32, x)
-            let lengthPtr := add(_bytes.offset, calldataload(add(_bytes.offset, shl(5, _arg))))
-            length := calldataload(lengthPtr)
-            offset := add(lengthPtr, 0x20)
-            relativeOffset := sub(offset, _bytes.offset)
-        }
-        if (_bytes.length < length + relativeOffset) revert SliceOutOfBounds();
-    }
-
-    /// @notice Decode the `_arg`-th element in `_bytes` as `bytes`
-    /// @param _bytes The input bytes string to extract a bytes string from
-    /// @param _arg The index of the argument to extract
-    function toBytes(bytes calldata _bytes, uint256 _arg) internal pure returns (bytes calldata res) {
-        (uint256 length, uint256 offset) = toLengthOffset(_bytes, _arg);
-        assembly {
-            res.length := length
-            res.offset := offset
-        }
-    }
-
     /// @notice Decode the `_arg`-th element in `_bytes` as `address[]`
     /// @param _bytes The input bytes string to extract an address array from
     /// @param _arg The index of the argument to extract
     function toAddressArray(bytes calldata _bytes, uint256 _arg) internal pure returns (address[] calldata res) {
-        (uint256 length, uint256 offset) = toLengthOffset(_bytes, _arg);
-        assembly {
-            res.length := length
-            res.offset := offset
-        }
-    }
-
-    /// @notice Decode the `_arg`-th element in `_bytes` as `bytes[]`
-    /// @param _bytes The input bytes string to extract a bytes array from
-    /// @param _arg The index of the argument to extract
-    function toBytesArray(bytes calldata _bytes, uint256 _arg) internal pure returns (bytes[] calldata res) {
-        (uint256 length, uint256 offset) = toLengthOffset(_bytes, _arg);
+        (uint256 length, uint256 offset) = _bytes.toLengthOffset(_arg);
         assembly {
             res.length := length
             res.offset := offset
@@ -96,10 +53,16 @@ library BytesLib {
     /// @param _bytes The input bytes string to extract an uint array from
     /// @param _arg The index of the argument to extract
     function toUintArray(bytes calldata _bytes, uint256 _arg) internal pure returns (uint256[] calldata res) {
-        (uint256 length, uint256 offset) = toLengthOffset(_bytes, _arg);
+        (uint256 length, uint256 offset) = _bytes.toLengthOffset(_arg);
         assembly {
             res.length := length
             res.offset := offset
         }
+    }
+
+    /// @notice Equivalent to abi.decode(bytes, bytes[])
+    /// @param _bytes The input bytes string to extract an parameters from
+    function decodeCommandsAndInputs(bytes calldata _bytes) internal pure returns (bytes calldata, bytes[] calldata) {
+        return _bytes.decodeActionsRouterParams();
     }
 }
