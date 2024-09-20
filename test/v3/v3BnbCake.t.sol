a204bb1ab4f2c912b53e9540a2751191c8b0aa9c
ChefMist <133624774+ChefMist@users.noreply.github.com>
2024-09-20 11:31:31 +0800

    test4 test5: add v3 swap related test

--- test/v3/v3BnbCake.t.sol
+++ test/v3/v3BnbCake.t.sol
@@ -9,12 +9,20 @@ import {PancakeSwapV3Test} from "./PancakeSwapV3.t.sol";
 contract V3BnbCake is PancakeSwapV3Test {
     ERC20 constant CAKE = ERC20(0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82);
 
+    // token1-token2 at 0.25%
+    // WETH9-BTCB: https://bscscan.com/address/0xfc75f4e78bf71ed5066db9ca771d4ccb7c1264e0
+    ERC20 constant BTCB = ERC20(0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c);
+
     function token0() internal pure override returns (address) {
         return address(CAKE);
     }
 
     function token1() internal pure override returns (address) {
         return address(WETH9);
+    }
+
+    function token2() internal pure override returns (address) {
+        return address(BTCB);
     }
 
     function fee() internal pure override returns (uint24) {
